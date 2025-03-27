d266f9d5de767bc6c529a79f1a0ba2788ba335a0
IshtyMahin <ishty119@gmail.com>
2025-03-28 02:39:07 +0600

    testing

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Enable standalone mode for better serverless function support
+  output: "standalone",
 };
 
 module.exports = nextConfig;
