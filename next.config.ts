410c049f38ff2e66bc429830e01e639f1042f26f
IshtyMahin <ishty119@gmail.com>
2025-03-28 01:58:10 +0600

    add netlify.toml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  target: "experimental-serverless-trace",
 };
 
 export default nextConfig;
